fe80086b5d7a4c1486cdd69d7479ce5d9909535e
likai <likai@bilibili.com>
2021-08-11 20:31:20 +0800

    seconds

--- main.cc
+++ main.cc
@@ -1,29 +1,72 @@
-
-
 /*
-½âÎöMPEG-TS
+è§£æMPEG-TS
 
-1.´Ó¸´ÓÃµÄMPEG-TSÁ÷ÖĞ½âÎö³öTS°ü£»
-2.´ÓTS°üÖĞ»ñÈ¡PAT¼°¶ÔÓ¦µÄPMT£¨PSIÖĞµÄ±í¸ñ£©£»
-3.´Ó¶ø»ñÈ¡ÌØ¶¨½ÚÄ¿µÄÒôÊÓÆµPID£»
-4.Í¨¹ıPIDÉ¸Ñ¡³öÌØ¶¨ÒôÊÓÆµÏà¹ØµÄTS°ü£¬²¢½âÎö³öPES£»
-5.´ÓPESÖĞ¶ÁÈ¡µ½PTS/DTS£¬²¢´ÓPESÖĞ½âÎö³ö»ù±¾ÂëÁ÷ES£»
-6.½«ES½»¸ø½âÂëÆ÷£¬»ñµÃÑ¹ËõÇ°µÄÔ­Ê¼ÒôÊÓÆµÊı¾İ
+1.ä»å¤ç”¨çš„MPEG-TSæµä¸­è§£æå‡ºTSåŒ…ï¼›
+2.ä»TSåŒ…ä¸­è·å–PATåŠå¯¹åº”çš„PMTï¼ˆPSIä¸­çš„è¡¨æ ¼ï¼‰ï¼›
+3.ä»è€Œè·å–ç‰¹å®šèŠ‚ç›®çš„éŸ³è§†é¢‘PIDï¼›
+4.é€šè¿‡PIDç­›é€‰å‡ºç‰¹å®šéŸ³è§†é¢‘ç›¸å…³çš„TSåŒ…ï¼Œå¹¶è§£æå‡ºPESï¼›
+5.ä»PESä¸­è¯»å–åˆ°PTS/DTSï¼Œå¹¶ä»PESä¸­è§£æå‡ºåŸºæœ¬ç æµESï¼›
+6.å°†ESäº¤ç»™è§£ç å™¨ï¼Œè·å¾—å‹ç¼©å‰çš„åŸå§‹éŸ³è§†é¢‘æ•°æ®
 
 */
 
 
+#include <_types/_uint16_t.h>
+#include <_types/_uint32_t.h>
+#include <_types/_uint64_t.h>
+#include <_types/_uint8_t.h>
+#include <cstdint>
+#include <cstdio>
 #include <stdint.h>
 #include <assert.h>
 #include <string.h>
+#include <sys/_types/_int64_t.h>
+#include <sys/_types/_size_t.h>
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 using namespace std;
 
 #define TS_PACKET_SIZE 188
 #define TS_SYNC_BYTE 0x47
+
+typedef struct {
+    unsigned int adaptation_field_length : 8;
+	unsigned int discontinuity_indicator : 1;
+	unsigned int random_access_indicator : 1;
+	unsigned int elementary_stream_priority_indicator : 1;
+	unsigned int PCR_flag : 1;
+	unsigned int OPCR_flag : 1;
+	unsigned int splicing_point_flag : 1;
+	unsigned int transport_private_data_flag : 1;
+	unsigned int adaptation_field_extension_flag : 1;
+
+	int64_t program_clock_reference_base; // 33-bits
+	unsigned int program_clock_reference_extension; // 9-bits
+
+	int64_t original_program_clock_reference_base; // 33-bits
+	unsigned int original_program_clock_reference_extension; // 9-bits
+
+	unsigned int splice_countdown : 8;
+
+	unsigned int transport_private_data_length : 8;
+
+	unsigned int adaptation_field_extension_length : 8;
+	unsigned int ltw_flag : 1;
+	unsigned int piecewise_rate_flag : 1;
+	unsigned int seamless_splice_flag : 1;
+
+	unsigned int ltw_valid_flag : 1;
+	unsigned int ltw_offset : 15;
+
+	unsigned int piecewise_rate : 22;
+
+	unsigned int Splice_type : 4;
+	int64_t DTS_next_AU;
+} ts_adaptation_field;
+
 
 typedef struct {
 	uint16_t transport_error_indicator : 1;
@@ -33,12 +76,13 @@ typedef struct {
 	uint8_t transport_scrambling_control : 2;
 	uint8_t adaptation_field_control : 2;
 	/*
-	'00' = ±£Áô (¹©Î´À´Ê¹ÓÃ)
-	'01' = ÎŞÊÊÅäÓò£¬½öÓĞÔØºÉ
-	'10' = ½öÓĞÊÊÅäÓò
-	'11' = ÊÊÅäÓòºÍÔØºÉ¶¼´æÔÚ
+	'00' = ä¿ç•™ (ä¾›æœªæ¥ä½¿ç”¨)
+	'01' = æ— é€‚é…åŸŸï¼Œä»…æœ‰è½½è·
+	'10' = ä»…æœ‰é€‚é…åŸŸ
+	'11' = é€‚é…åŸŸå’Œè½½è·éƒ½å­˜åœ¨
 	*/
 	uint8_t continuity_counter : 4;
+    ts_adaptation_field adaptation;
 } ts_header;
 
 
@@ -57,10 +101,53 @@ typedef struct {
 
 
 typedef struct {
+    uint8_t sid;
+    uint8_t stream_type;
+
+    int flags;
+    int64_t pts;
+    int64_t dts;
+    vector<char> data;
+} ts_packet;
+
+
+typedef struct {
+    uint16_t program_number;
 	unsigned stream_type : 8;
 	unsigned elementary_PID : 13;
 	unsigned ES_info_length : 12;
 	unsigned descriptor;
+    uint8_t sid;
+    uint8_t cc;
+    uint32_t len;
+    
+    // optional packet length
+    uint32_t reversed10:2;
+    uint32_t PES_scrambling_control:2;
+    uint32_t PES_prioriy:1;
+    uint32_t data_alignment_indicator:1;
+    uint32_t copyright:1;
+    uint32_t original_or_copy:1;
+    
+    // 7ä¸ªflag
+    uint32_t PTS_DTS_flags : 2;
+	uint32_t ESCR_flag : 1;
+	uint32_t ES_rate_flag : 1;
+	uint32_t DSM_trick_mode_flag : 1;
+	uint32_t additional_copy_info_flag : 1;
+	uint32_t PES_CRC_flag : 1;
+	uint32_t PES_extension_flag : 1;
+
+    uint32_t PES_header_data_length:8;
+    
+    int64_t pts;
+    int64_t dts;
+    int64_t ESCR_base;
+    uint32_t ESCR_extension;
+    uint32_t ES_rate;
+
+
+    ts_packet pkt;
 } ts_pes;
 
 typedef struct {
@@ -100,9 +187,84 @@ enum ETS_PID {
 	TS_PID_SDT =  0x11
 };
 
+enum STREAM_TYPE {
+    PSI_STREAM_H264 = 0x1b,
+    PSI_STREAM_H265 = 0x24
+};
+
 size_t pat_read(ts_pat* pat, const uint8_t* data, size_t bytes);
 size_t sdt_read(ts_pat* pat, const uint8_t* data, size_t bytes);
 size_t pmt_read(ts_pmt* pmt, const uint8_t* data, size_t bytes);
+size_t pes_read_header(ts_pes* pes, const uint8_t* data, size_t bytes);
+
+
+int pes_packet(ts_packet *pkt, const ts_pes *pes, const uint8_t *data, size_t size, int start) {
+
+    if (pes->stream_type == PSI_STREAM_H264 || pes->stream_type == PSI_STREAM_H265) {
+    }
+    else {
+    }
+    return 0;
+}
+
+
+static uint32_t adaptation_field_read(ts_adaptation_field *adp, const uint8_t *data, size_t bytes) {
+    uint32_t i = 0;
+    uint32_t j = 0;
+
+    assert(bytes <= TS_PACKET_SIZE);
+    adp->adaptation_field_length = data[i++];
+    if (adp->adaptation_field_length > 0) {
+        adp->discontinuity_indicator = (data[i] >> 7) & 0x01;
+        adp->random_access_indicator = (data[i] >> 6) & 0x01;
+        adp->elementary_stream_priority_indicator = (data[i] >> 5) & 0x01;
+        adp->PCR_flag = (data[i] >> 4) & 0x01;
+        adp->OPCR_flag = (data[i] >> 3) & 0x01;
+        adp->splicing_point_flag = (data[i] >> 2) & 0x01;
+        adp->transport_private_data_flag = (data[i] >> 1) & 0x01;
+        adp->adaptation_field_extension_flag = (data[i] >> 0) & 0x01;
+        i++;
+    }
+    if (adp->PCR_flag) {
+        adp->program_clock_reference_base = ((uint64_t)data[i] << 25) | ((uint64_t)data[i+1] << 17) | ((uint64_t)data[i+2] << 9) | ((uint64_t)data[i+3] << 1) | ((data[i+4] >> 7) & 0x01);
+        adp->program_clock_reference_extension = ((data[i+4] & 0x01) << 8) | data[i+5];
+        i += 6;
+    }
+    if (adp->OPCR_flag) {
+        adp->original_program_clock_reference_base = (((uint64_t)data[i]) << 25) | ((uint64_t)data[i+1] << 17) | ((uint64_t)data[i+2] << 9) | ((uint64_t)data[i+3] << 1) | ((data[i+4] >> 7) & 0x01);
+        adp->original_program_clock_reference_extension = ((data[i+4] & 0x01) << 1) | data[i+5];
+        i += 6;
+    }
+    if (adp->splicing_point_flag) {
+        adp->splice_countdown = data[i++];
+    }
+    if (adp->transport_private_data_flag) {
+        adp->transport_private_data_length = data[i++];
+        for (j = 0; j < adp->transport_private_data_length; j++) {
+            // transport_private_data
+        }
+        i += adp->transport_private_data_length;
+    }
+    if (adp->adaptation_field_extension_flag) {
+        adp->adaptation_field_extension_length = data[i++];
+        adp->ltw_flag = (data[i] >> 7) & 0x01;
+        adp->piecewise_rate_flag = (data[i] >> 6) & 0x01;
+        adp->piecewise_rate_flag = (data[i] >> 5) * 0x01;
+        
+        i++;
+        if (adp->ltw_flag) {
+            i += 2;
+        }
+        if (adp->piecewise_rate_flag) {
+            i += 3;
+        }
+        if (adp->seamless_splice_flag) {
+            i += 5;
+        }
+    }
+    return adp->adaptation_field_length + 1;   
+} 
+
 
 int ts_demuxer_input(ts_demuxer *ts, const uint8_t* data, size_t bytes) {
 	assert(188 == bytes);
@@ -123,20 +285,21 @@ int ts_demuxer_input(ts_demuxer *ts, const uint8_t* data, size_t bytes) {
 	pkhd.continuity_counter = (data[3]) & 0x0F;
 
 	if ((ts->cc + 1) % 15 != (uint8_t)pkhd.continuity_counter) {
-		// Èç¹û°ü²»ÊÇÁ¬ĞøµÄ 
+		// å¦‚æœåŒ…ä¸æ˜¯è¿ç»­çš„ 
 	}
 
 	ts->cc = (uint8_t)pkhd.continuity_counter;
 
-	// ´òÓ¡³öÃ¿¸ö°üµÄÍ·²¿ĞÅÏ¢
+	// æ‰“å°å‡ºæ¯ä¸ªåŒ…çš„å¤´éƒ¨ä¿¡æ¯
 
 	i = 4;
-	if (0x02 == pkhd.adaptation_field_control) {
-		// ½öÓĞÊÊÓ¦Óò
+	if (0x02 & pkhd.adaptation_field_control) {
+		// ä»…æœ‰é€‚åº”åŸŸ
+        i += adaptation_field_read(&pkhd.adaptation, data + i, bytes - i);
 	}
 
-	if (0x01 == pkhd.adaptation_field_control) {
-		// ½öÓĞ¸ºÔØ
+	if (0x01 & pkhd.adaptation_field_control) {
+		// ä»…æœ‰è´Ÿè½½
 		switch (PID)
 		{
 		case TS_PID_PAT:
@@ -170,7 +333,29 @@ int ts_demuxer_input(ts_demuxer *ts, const uint8_t* data, size_t bytes) {
 						i += 1;
 					}
 					pmt_read(&ts->pat.pat_programs[j], data + i, bytes - i);
-				}
+                    break;
+                } else {
+                    for (uint32_t k = 0; k < ts->pat.pat_programs[j].stream_count; k++) {
+                        ts_pes * pes = &ts->pat.pat_programs[j].pmt_streams[k];
+                        if (PID != pes->elementary_PID) {
+                            continue;
+                        }
+
+                        if (pkhd.payload_unit_start_indicator) {
+                            size_t n;
+                            n = pes_read_header(pes, data + i, bytes - i);
+                            assert(n > 0);
+                            i += n;
+                        }
+                        else if (0 == pes->sid) {
+                            continue;
+                        }
+
+                        // pes_packet(&pes->pkt, pes, data + i, bytes - i, pkhd.payload_unit_start_indicator);
+                        break;
+                    }      
+                }
+
 			}
 			break;
 		}
@@ -199,7 +384,7 @@ size_t sdt_read(ts_pat* pat, const uint8_t* data, size_t bytes) {
 
 		uint32_t p = 0;
 		for (; p < pat->pmt_count; p++) {
-			if (pat->pat_programs[p].program_number = service_id) {
+			if (pat->pat_programs[p].program_number == service_id) {
 				break;
 			}
 		}
@@ -245,6 +430,7 @@ size_t pat_read(ts_pat* pat, const uint8_t* data, size_t bytes) {
 	uint32_t transport_stream_id = (data[3] << 8) | data[4];
 	uint32_t version_number = (data[5] >> 1) & 0x1F;
 
+
 	assert(table_id == 0x00);
 	assert(section_syntax_indicator == 1);
 
@@ -263,13 +449,13 @@ size_t pat_read(ts_pat* pat, const uint8_t* data, size_t bytes) {
 	{
 		pn = (data[i] << 8) | data[i + 1];
 		pid = ((data[i + 2] & 0x1F) << 8) | data[i + 3];
-		// ´òÓ¡³öÀ´PAT´æ´¢µÄ½ÚÄ¿ĞÅÏ¢
+		// æ‰“å°å‡ºæ¥PATå­˜å‚¨çš„èŠ‚ç›®ä¿¡æ¯
 		if (pn == 0)
 		{
-			// ½ÚÄ¿ºÅÎª0x0000 ±íÊ¾ÕâÊÇNIT
+			// èŠ‚ç›®å·ä¸º0x0000 è¡¨ç¤ºè¿™æ˜¯NIT
 			continue;
 		}
-		// pn = 0x0001 ±íÊ¾ÕâÊÇ PMT
+		// pn = 0x0001 è¡¨ç¤ºè¿™æ˜¯ PMT
 		ts_pmt prg;
 		prg.program_number = pn;
 		prg.pid = pid;
@@ -277,7 +463,7 @@ size_t pat_read(ts_pat* pat, const uint8_t* data, size_t bytes) {
 		pat->pmt_count++;
 	}
 
-	// ×îºÃÔÙÑéÖ¤Ò»ÏÂCRC32
+	// æœ€å¥½å†éªŒè¯ä¸€ä¸‹CRC32
 
 	return 0;
 }
@@ -296,7 +482,7 @@ size_t pmt_read(ts_pmt* pmt, const uint8_t* data, size_t bytes) {
 	uint32_t PCR_PID = ((data[8] & 0x1F) << 8) | data[9];
 	uint32_t program_info_length = ((data[10] & 0x0F) << 8) | data[11];
 
-	// PMT ±íµÄ table id Ò»¶¨ÊÇ 0x02
+	// PMT è¡¨çš„ table id ä¸€å®šæ˜¯ 0x02
 	assert(0x02 == table_id);
 	assert(1 == section_syntax_indicator);
 
@@ -307,7 +493,7 @@ size_t pmt_read(ts_pmt* pmt, const uint8_t* data, size_t bytes) {
 	pmt->PCR_PID = PCR_PID;
 	pmt->program_number = program_number;
 	pmt->pminfo_len = program_info_length;
-
+    pmt->version_number = version_number;
 
 	int len = 0;
 	int pos = 12;
@@ -315,51 +501,123 @@ size_t pmt_read(ts_pmt* pmt, const uint8_t* data, size_t bytes) {
 		// descriptor
 		
 	}
-	pos += program_info_length; /* program info length ºóÃæÊÇ descriptor ÒªÌø¹ı */
+	pos += program_info_length; /* program info length åé¢æ˜¯ descriptor è¦è·³è¿‡ */
 	unsigned int pid, stream_type, desc;
 	for (; pos + 5 <= section_length + 12 - 9/*section item*/ - 4/*CRC32*/; pos += len + 5) {
 		stream_type = data[pos];
 		pid = ((data[pos + 1] & 0x1F) << 8) | data[pos + 2];
 		len = ((data[pos + 3] & 0x0F) << 8) | data[pos + 4];
-		desc = 0x00;
+        if (pos + len + 5 > section_length + 3 - 4) break;
+        desc = 0x00;
 		if (len != 0) {
 			// descriptor
-			// ÕâÀïºöÂÔ
+			// è¿™é‡Œå¿½ç•¥
 		}
 		ts_pes st;
-		st.stream_type = stream_type;
+        st.program_number = (uint16_t)pmt->program_number;
+		// ç¼–ç å™¨ç±»å‹
+        st.stream_type = stream_type;
 		st.elementary_PID = pid;
-		st.ES_info_length = len;
+		st.ES_info_length = 0; // default nothing
 		st.descriptor = desc;
+        st.sid = 0;
 		pmt->pmt_streams.push_back(st);
+        pmt->stream_count++;
 	}
 
 	return 0;
 }
 
+#define DATA(off) data_read_with_bodunary_check(data, bytes, off, &overflow)
+static inline uint8_t data_read_with_bodunary_check(const uint8_t *data, size_t bytes, size_t off, int *overflow) {
+    return (off < bytes) ? (data[off]) : (*overflow = 1, 0);
+}
+
 
 size_t pes_read_header(ts_pes* pes, const uint8_t* data, size_t bytes) {
-	return 0;
+    size_t i;
+    int overflow;
+    
+    overflow = 0;
+    if (bytes < 3 + 6) return 0;
+
+    // prefix
+    assert(0x00 == data[0] && 0x00 == data[1] && 0x01 == data[2]);
+    pes->sid = data[3];
+    pes->len = (data[4] << 8) | data[5];
+    i = 6;
+    assert(0x02 == (DATA(i) >> 6 & 0x03));
+    pes->PES_scrambling_control = (DATA(i) >> 4) & 0x03;
+    pes->PES_prioriy = (DATA(i) >> 3) & 0x01;
+    pes->data_alignment_indicator = (DATA(i) >> 2) & 0x01;
+    pes->copyright = (DATA(i) >> 1) & 0x01;
+    pes->original_or_copy = (DATA(i) & 0x01);
+    
+    i++;
+    pes->PTS_DTS_flags = (DATA(i) >> 6) & 0x03;
+    pes->ESCR_flag = (DATA(i) >> 5) & 0x01;
+    pes->ES_rate_flag = (DATA(i) >> 4) & 0x01;
+    pes->DSM_trick_mode_flag = (DATA(i) >> 3) &0x01;
+    pes->additional_copy_info_flag = (DATA(i) >> 2) & 0x01;
+    pes->PES_CRC_flag = (DATA(i) >> 1) & 0x01;
+    pes->PES_extension_flag = (DATA(i) & 0x01);
+
+    i++;
+    pes->PES_header_data_length = DATA(i);
+
+    i++;
+    if (0x02 & pes->PTS_DTS_flags) {
+        // PTS
+        // åˆ¤æ–­æ˜¯å¦æœ‰æ•°æ®
+        assert(0x20 == (DATA(i) & 0x20));
+        pes->pts = ((((uint64_t)DATA(i) >> 1) & 0x07) << 30) | ((uint64_t)DATA(i + 1) << 22) | ((((uint64_t)DATA(i + 2) >> 1) & 0x7F) << 15) | ((uint64_t)DATA(i + 3) << 7) | ((DATA(i + 4) >> 1) & 0x7F);
+        i += 5;
+    }
+    if (0x01 & pes->PTS_DTS_flags) {
+        // DTS
+        assert(0x10 == (DATA(i) & 0x10));
+        pes->dts = ((((uint64_t)DATA(i) >> 1) & 0x07) << 30) | ((uint64_t)DATA(i + 1) << 22) | ((((uint64_t)DATA(i + 2) >> 1) & 0x7F) << 15) | ((uint64_t)DATA(i + 3) << 7) | ((DATA(i + 4) >> 1) & 0x7F);
+        i += 5;
+    } else if (0x02 & pes->PTS_DTS_flags) {
+        pes->dts = pes->pts;  // å¦‚æœåªæœ‰ 0x10 çš„è¯  é‚£è¯´æ˜ æ²¡æœ‰dtsçš„ä½ç½®  é‚£å°±æ˜¯ DTS=PTS
+    }
+
+    if (pes->DSM_trick_mode_flag) i+=1;
+    if (pes->additional_copy_info_flag) i+=1;
+    if (pes->PES_CRC_flag) i += 2;
+    if (pes->PES_extension_flag) {}
+    if (pes->len) {
+        if (pes->len < pes->PES_header_data_length + 3) {
+            return 0;
+        }
+        pes->len -= pes->PES_header_data_length + 3;
+    }
+    assert(pes->len >= 0);
+
+	return overflow ? 0 : pes->PES_header_data_length + 9;
 }
 
 
 void read_file(const char* path) {
 	FILE *fp = fopen(path, "r");
 	if (fp == NULL) {
-		printf("open %s failed, errno=%d\n", fp, errno);
+		printf("open %s failed, errno=%d\n", path, errno);
 		return;
 	}
 	ts_demuxer muxer;
 	memset(&muxer, 0, sizeof(muxer));
 	char buf[188] = { 0 };
-	while (true)
+	int count = 0;
+    while (true)
 	{
 		size_t n = fread(buf, 1, sizeof(buf), fp);
 		if (feof(fp)) break;
 		assert(n == 188);
 		ts_demuxer_input(&muxer, (uint8_t*)buf, n);
+        count++;
 	}
 	fclose(fp);
+    printf("packet count = %d\n", count);
 }
 
 
